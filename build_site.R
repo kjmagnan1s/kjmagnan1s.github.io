92d672a6c307a4b8174884a907ce6cc1505309a0
Kevin Magnan <Kevin Magnan>
2017-04-13 22:31:39 -0500

    added projects and custom css for images

--- build_site.R
+++ build_site.R
@@ -2,7 +2,8 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:\Users\kjmag\Documents\GitHub\kjmagnan1s.github.io")
+setwd("C:/Users/kjmag/Documents/Git/kjmagnan1s")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
