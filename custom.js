ed41334dcd944390b1dabccef11e77283ccae51b
Kevin Magnan <kjmagnan1s@gmail.com>
2025-02-14 16:12:08 -0600

    add/remove pages, update twitter to bluesky footer

--- custom.js
+++
@@ -1,9 +0,0 @@
-// custom.js
-function toggleMenu() {
-  var menu = document.getElementById("nav-menu");
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
-}
